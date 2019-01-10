46c981338bd1ad0996936767fa859b90467c8955
T-kumaX <isobetakuma@MacBook-Pro-of-Tkuma.local>
2019-01-10 19:47:51 +0900

    set branch which use for depoy

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,10 +8,11 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
-
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/T-kumaX_suisui07boy25_keypair.pem']
 
+
+set :branch, ENV['BRANCH']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
